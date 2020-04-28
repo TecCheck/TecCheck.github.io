8847106c04a9a94771f5da2b654f24a2cab7b079
Chris W <mcteccy@gmail.com>
2020-04-28 21:01:37 +0200

    added dark night switch and german translation

--- script/main.js
+++ script/main.js
@@ -1,5 +1,72 @@
-import { MDCTopAppBar } from '@material/top-app-bar';
+const MDCIconButtonToggle = mdc.iconButton.MDCIconButtonToggle;
+const MDCMenu = mdc.menu.MDCMenu;
 
-// Instantiation
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
+var darkmode = false;
+
+var darkNightSwitch;
+var languageMenu;
+var languageMenuButton;
+
+var languageMenuEnglish;
+var languageMenuGerman;
+
+function load() {
+    var darkNightButton = document.getElementById("dark-night-switch")
+
+    darkNightSwitch = new MDCIconButtonToggle(darkNightButton);
+    languageMenu = new MDCMenu(document.getElementById("language-menu"));
+    languageMenuButton = document.getElementById("language-menu-button");
+
+    languageMenuEnglish = document.getElementById("language-menu-english");
+    languageMenuGerman = document.getElementById("language-menu-german");
+
+    languageMenuButton.addEventListener("click", function () {
+        languageMenu.open = true;
+    });
+
+    languageMenuEnglish.addEventListener("click", function () {
+        document.location.href = document.location.origin;
+    });
+
+    languageMenuGerman.addEventListener("click", function () {
+        document.location.href = document.location.origin + "/de";
+    });
+
+    darkNightButton.addEventListener("click", function () {
+        updateDark();
+    });
+
+    darkNightSwitch.on = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    updateDark();
+}
+
+function updateDark() {
+    if(darkNightSwitch.on)
+        loadDarkCss();
+    else
+        unloadDarkCss();
+}
+
+function loadDarkCss() {
+    var cssId = "dark-css";
+    if (!document.getElementById(cssId)) {
+        var head = document.getElementsByTagName('head')[0];
+        var link = document.createElement('link');
+        link.id = cssId;
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = document.location.origin + "/css/style-dark.css";
+        link.media = 'all';
+        head.appendChild(link);
+    }
+}
+
+function unloadDarkCss() {
+    var css = document.getElementById("dark-css");
+    if (css)
+        css.remove();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    load();
+});
